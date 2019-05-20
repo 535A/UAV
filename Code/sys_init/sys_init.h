43769e74e771d214e7d0747a2210a7cc894660ab
535A <50834971+535A@users.noreply.github.com>
2019-05-20 22:23:08 +0800

    Revert "Initial commit"
    This reverts commit b3c76cfbc30facdef80c11addfb0253efe106e1c.

--- Code/sys_init/sys_init.h
+++
@@ -1,9 +0,0 @@
-#ifndef SYS_INIT_H
-#define SYS_INIT_H
-
-void sys_config();
-void TIM2_init(u16 arr,u16 psc);
-void TIM1_PWM_init(u16 arr,u16 psc);
-#define COUNT PBout(1)	
-#define NRFCOUNT PBout(3)	
-#endif
